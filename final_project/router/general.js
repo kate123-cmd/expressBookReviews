aa4b531d35bc6f66c062e72dfdb5fe0ecf8504b0
kate123-cmd <kashasingh22@gmail.com>
2026-05-09 06:21:14 -0400

    get book review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,44 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+let users = [];
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const doesExist = (username) => {
+    let usersWithSameName = users.filter((user) => {
+        return user.username === username;
+    });
+
+    return usersWithSameName.length > 0;
+};
+
+public_users.post("/register", (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Check if username and password are provided
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Username or password missing"
+        });
+    }
+
+    // Check if user already exists
+    if (doesExist(username)) {
+        return res.status(409).json({
+            message: "User already exists!"
+        });
+    }
+
+    // Register new user
+    users.push({
+        username: username,
+        password: password
+    });
+
+    return res.status(200).json({
+        message: "User successfully registered. Now you can login"
+    });
 });
 
 // Get the book list available in the shop
@@ -57,8 +91,20 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Get ISBN from request parameters
+  const isbn = req.params.isbn;
+
+  // Check if book exists
+  if (books[isbn]) {
+
+      // Return reviews of the book
+      return res.send(JSON.stringify(books[isbn].reviews));
+  }
+
+  // If ISBN not found
+  return res.status(404).json({
+      message: "Book not found"
+  });
 });
 
 module.exports.general = public_users;
